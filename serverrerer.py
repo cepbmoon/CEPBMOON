be2e41e0ee9bff7d54f2430442dc0c588865a587
cepbmoon <cepbmoon@gmail.com>
2026-07-08 09:31:12 -0300

    Add files via upload

--- serverrerer.py
+++ serverrerer.py
@@ -2,21 +2,31 @@ from flask import *
 import psycopg2
 
 app = Flask(__name__)
-conn = psycopg2.connect#(database=db_CEPBMOON,
-                        #user=DB_USER,
-                        #password=DB_PASS,
-                        #host=DB_HOST,
-                        #port=DB_PORT)
+conn = psycopg2.connect(database="db_CEPBMOON",
+                        user="avnadmin",
+                        host="cepbmoon-cepb-moon.c.aivencloud.com",
+                        port=27526,
+                        sslmode="require")
 cursor = conn.cursor()
+cursor.execute("SHOW TABLES;")
+print(cursor.fetchall())
 
-@app.get("/lista")
-def listar():
+@app.get("/fila_delegaciones")
+def obtener_botones():
+
     cursor.execute("""
-SELECT 
+        SELECT texto
+        FROM botones
+        WHERE sesion_id = %s
+    """, (...))
+
+    return [...]
+
+@app.get("/historial_intervenciones")
+
+@app.get("/historial_")
 
 
-""")
-    
 @app.post("/")
 def publicar(str):
     cursor.execute(str)
